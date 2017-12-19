9f2838a01c4f9f6ae8be138410c79b646a4d5707
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-19 02:31:56 +0100

    Reading and displaying image file

--- opencv-img-manipulations.py
+++ opencv-img-manipulations.py
@@ -2,8 +2,10 @@ import cv2
 
 # Reading an image file
 image = cv2.imread('images/jurassic-park-tour-jeep.jpg')
+
 # displaying image
 cv2.imshow('Original', image)
+
 # press any key to exit...
 cv2.waitKey(0)
 cv2.destroyAllWindows()
