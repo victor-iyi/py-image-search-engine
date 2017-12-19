e39ce67f2d2396d59d2c3410d374558b4ec7424b
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-19 02:46:19 +0100

    Resizing image

--- opencv-img-manipulations.py
+++ opencv-img-manipulations.py
@@ -1,11 +1,22 @@
 import cv2
 
 # Reading an image file
-image = cv2.imread('images/jurassic-park-tour-jeep.jpg')
+img = cv2.imread('images/jurassic-park-tour-jeep.jpg')
+
+
+print(f'Image shape: {img.shape}')
 
 # displaying image
-cv2.imshow('Original', image)
+cv2.imshow('Original', img)
 
-# press any key to exit...
+# Resizing image
+new_width = 250
+ratio = new_width / img.shape[1]  # 100 / width
+dim = (new_width, int(img.shape[0] * ratio))
+
+resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+cv2.imshow('Resized', resized)
+
+# Press any key to exit...
 cv2.waitKey(0)
 cv2.destroyAllWindows()
