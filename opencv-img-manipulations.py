accaa8a117d6843c76695a1ff081d6d41db1feaa
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-20 20:04:04 +0100

    OpenCV Image Manipulations

--- opencv-img-manipulations.py
+++ opencv-img-manipulations.py
@@ -1,21 +1,2 @@
-import cv2
+img_file = 'images/jurassic-park-tour-jeep.jpg'
 
-# Reading an image file
-img = cv2.imread('images/jurassic-park-tour-jeep.jpg')
-
-print(f'Image shape: {img.shape}')
-
-# displaying image
-cv2.imshow('Original', img)
-
-# Resizing image
-new_width = 250
-ratio = new_width / img.shape[1]  # 100 / width
-dim = (new_width, int(img.shape[0] * ratio))
-
-resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-cv2.imshow('Resized', resized)
-
-# Press any key to exit...
-cv2.waitKey(0)
-cv2.destroyAllWindows()
