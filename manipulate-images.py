e4b3032f2322f6ecd5ff325cebf179ebb38bd92a
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-20 20:17:42 +0100

    Resized image

--- manipulate-images.py
+++ manipulate-images.py
@@ -17,11 +17,16 @@ img_original = cv2.imread(img_file)
 img_original_shape = img_original.shape
 print(f'Original image shape = {img_original_shape}')
 
-# Resize dimensions
+# Resize dimensions [aspect ratio]
+# ratio of the new image to the old one
+r = 100 / img_original_shape[1]  # new_height / old_width
+dim = (100, int(img_original_shape[0] * r))  # [new_height, (old_height * ratio)]
 
+img_resized = cv2.resize(img_original, dim, interpolation=cv2.INTER_AREA)
 
 # Display images
 cv2.imshow('Original', img_original)
+cv2.imshow('Resized', img_resized)
 
 # Press any key to exit...
 cv2.waitKey(0)
