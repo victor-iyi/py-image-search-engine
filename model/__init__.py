6ad485cc30500fe06687f76ace965d05eb174e1e
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-21 18:48:12 +0100

    model package: descriptor.py - Image Descriptor

---
+++ model/__init__.py
@@ -0,0 +1,9 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  
+  Created on 21 December, 2017 @ 6:47 PM.
+  
+  Copyright © 2017. Victor. All rights reserved.
+"""
