37fb999762719a06a701116f535ea7f13f0c6dcc
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-22 20:21:28 +0100

    Rotating image

--- manipulate-images.py
+++ manipulate-images.py
@@ -17,8 +17,11 @@ img_original = cv2.imread(img_file)
 img_original_shape = img_original.shape   # (height, width, channel)
 print(f'Original image shape = {img_original_shape}')
 
-# Resize dimensions [aspect ratio]
-# ratio of the new image to the old one
+################################################################################################
+# +———————————————————————————————————————————————————————————————————————————————————————————+
+# | Resizing images
+# +———————————————————————————————————————————————————————————————————————————————————————————+
+################################################################################################
 """
 We have to keep the aspect ratio of the image in mind, which is the 
 proportional relationship of the width and the height of the image. 
@@ -35,9 +38,22 @@ dim = (new_height, int(img_original_shape[0] * r))  # [new_height, (old_height *
 img_resized = cv2.resize(img_original, dim, interpolation=cv2.INTER_AREA)
 img_resized_shape = img_resized.shape
 print(f'Resized image shape = {img_resized_shape}')
+
+################################################################################################
+# +———————————————————————————————————————————————————————————————————————————————————————————+
+# | Rotating images
+# +———————————————————————————————————————————————————————————————————————————————————————————+
+################################################################################################
+(h, w) = img_original_shape[:2]
+center = (h/2, w/2)
+
+rotation_matrix = cv2.getRotationMatrix2D(center, 180, 1.0)
+img_rotated = cv2.warpAffine(img_original, rotation_matrix, (w, h))
+
 # Display images
 cv2.imshow('Original', img_original)
 cv2.imshow('Resized', img_resized)
+cv2.imshow('Rotated', img_rotated)
 
 # Press any key to exit...
 cv2.waitKey(0)
