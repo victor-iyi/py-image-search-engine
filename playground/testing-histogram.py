ebc194adde9ac3c42354d2205c8578bc8fcb619a
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-23 22:01:49 +0100

    Multiple channel's histogram

--- playground/testing-histogram.py
+++ playground/testing-histogram.py
@@ -10,6 +10,7 @@
 import argparse
 
 import cv2
+import numpy as np
 import matplotlib.pyplot as plt
 
 ################################################################################################
@@ -20,7 +21,7 @@ import matplotlib.pyplot as plt
 parser = argparse.ArgumentParser()
 # Arguments
 parser.add_argument('-i', '--image',
-                    default='../images/jurassic-park/grant.jpg',
+                    default='../images/jurassic-park/jurassic-park-tour-jeep.jpg',
                     help='Image to find it\'s color histogram')
 args = parser.parse_args()
 
@@ -30,25 +31,45 @@ args = parser.parse_args()
 # +———————————————————————————————————————————————————————————————————————————————————————————+
 ################################################################################################
 
-img = cv2.imread(args.image)
-# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-hist = cv2.calcHist(images=[img], channels=[0], mask=None, histSize=[128], ranges=[0, 256])
+image = cv2.imread(args.image)
+# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+hist = cv2.calcHist(images=[image], channels=[0], mask=None, histSize=[256], ranges=[0, 256])
 
+
+################################################################################################
+# +———————————————————————————————————————————————————————————————————————————————————————————+
+# | Flattened color histogram
+# +———————————————————————————————————————————————————————————————————————————————————————————+
+################################################################################################
+features = []
+colors = ('b', 'g', 'r')
+channels = cv2.split(image)
+
+# Loop through each channel. one at a time
+# computing each channel's histogram
+for (channel, color) in zip(channels, colors):
+    channel_hist = cv2.calcHist([channel], channels=[0], mask=None, histSize=[256], ranges=[0, 256])
+    features.extend(channel_hist)  # extend list by appending elements 4rm d iterable
+    # Plot the histogram
+    plt.plot(channel_hist, color=color, label=f'Channel {color}', linewidth=1)
+    plt.xlim([0, 256])
+
+print('Flatten features shape = {}'.format(np.ravel(features).shape))
 ################################################################################################
 # +———————————————————————————————————————————————————————————————————————————————————————————+
 # | Plot the histogram & display image
 # +———————————————————————————————————————————————————————————————————————————————————————————+
 ################################################################################################
 # Plot histogram
-plt.figure()
-plt.plot(hist)
+plt.plot(hist, color='k', label='Image histogram', linewidth=1.5)
 plt.title('Color Histogram')
 plt.xlabel('# of Bins')
 plt.ylabel('Pixel values')
+plt.legend()
 plt.show()
 
 # Display image
-cv2.imshow('Original', img)
+# cv2.imshow('Original', image)
 # cv2.imshow('Grayscale', gray)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
