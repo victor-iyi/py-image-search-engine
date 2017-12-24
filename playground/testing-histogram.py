98888b541e707d7dac78da1160afb778c83d24af
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-24 22:00:56 +0100

    Update playground/testing-histogram.py

--- playground/testing-histogram.py
+++ playground/testing-histogram.py
@@ -14,29 +14,28 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Default images
+image_folder = '../images/jurassic-park/'
+default_images = [os.path.join(image_folder, img) for img in os.listdir(image_folder) if img[0] is not '.']
+
 ################################################################################################
 # +———————————————————————————————————————————————————————————————————————————————————————————+
 # | Command line arguments
 # +———————————————————————————————————————————————————————————————————————————————————————————+
 ################################################################################################
-# Default images
-image_folder = '../images/jurassic-park/'
-default_images = [img for img in os.listdir(image_folder) if img[0] is not '.']
-
-# Arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('-i', '--image',
                     default=default_images[0],
                     help='Image to find it\'s color histogram')
 args = parser.parse_args()
 # Add on the images folder
-args.image = os.path.join(image_folder, args.image)
+# args.image = os.path.join(image_folder, args.image)
+
 ################################################################################################
 # +———————————————————————————————————————————————————————————————————————————————————————————+
 # | Calculating the Histogram
 # +———————————————————————————————————————————————————————————————————————————————————————————+
 ################################################################################################
-
 image = cv2.imread(args.image)
 # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 hist = cv2.calcHist(images=[image], channels=[0], mask=None, histSize=[256], ranges=[0, 256])
@@ -74,7 +73,11 @@ plt.ylabel('Pixel values')
 plt.legend()
 plt.show()
 
-# Display image
+################################################################################################
+# +———————————————————————————————————————————————————————————————————————————————————————————+
+# | Display images
+# +———————————————————————————————————————————————————————————————————————————————————————————+
+################################################################################################
 # cv2.imshow('Original', image)
 # cv2.imshow('Grayscale', gray)
 # cv2.waitKey(0)
