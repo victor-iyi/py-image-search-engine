efd49c76f23a4aefde6b33c18e5255e60987b85e
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-25 20:48:05 +0100

    Step 2: Indexing

--- playground/image-search-engine-demo.py
+++ playground/image-search-engine-demo.py
@@ -7,8 +7,11 @@
   
   Copyright © 2017. Victor. All rights reserved.
 """
-import cv2
+import os
+import glob
 import argparse
+
+import cv2
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dataset', default='../images/lord-of-the-rings/',
@@ -19,13 +22,20 @@ args = parser.parse_args()
 
 
 class RGBHistogram:
+    """
+    Image descriptor using color histogram.
 
-    def __init__(self, bins=(8, 8, 8)):
-        """
-        Image descriptor using color histogram.
+    :param bins: list
+        Histogram size. 1-D list containing ideal values
+        between 8 and 128; but you can go up till 0 - 256.
 
-        :param bins: list,
-        """
+    Example:
+        >>> histogram = RGBHistogram(bins=[32, 32, 32])
+        >>> feature_vector = histogram.describe(image='folder/image.jpg')
+        >>> print(feature_vector.shape)
+    """
+
+    def __init__(self, bins):
         self.bins = bins
 
     def describe(self, image):
@@ -45,5 +55,13 @@ class RGBHistogram:
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         hist = cv2.calcHist(images=[image], channels=[0, 1, 2], mask=None,
                             histSize=self.bins, ranges=[0, 256] * 3)
-        hist = cv2.normalize(hist)
+        hist = cv2.normalize(hist, dst=hist.shape)
         return hist.flatten()
+
+
+# key - image file name, value - computed feature vector/descriptor
+index = {}
+descriptor = RGBHistogram(bins=[8, 8, 8])
+
+for image in glob.glob(os.path.join(args.dataset, '*.png')):
+    print(image)
