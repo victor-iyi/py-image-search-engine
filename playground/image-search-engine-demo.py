da244b3c163a746d677028c96e8a2c3100f9b21d
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-25 19:18:32 +0100

    Image Search Engine Demo

---
+++ playground/image-search-engine-demo.py
@@ -0,0 +1,9 @@
+"""
+  @author Victor I. Afolabi
+  A.I. Engineer & Software developer
+  javafolabi@gmail.com
+  
+  Created on 25 December, 2017 @ 7:18 PM.
+  
+  Copyright © 2017. Victor. All rights reserved.
+"""
