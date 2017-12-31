cd6f175bc5106235948e4e8a2bc1148bafc0c58b
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-31 08:46:23 +0100

    Added __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+"""
+  @author Victor I. Afolabi
+  A.I. engineer/researcher & Software engineer
+  javafolabi@gmail.com
+  
+  Created on 31 December, 2017 @ 8:46 AM.
+  
+  Copyright © 2017. Victor. All rights reserved.
+"""
