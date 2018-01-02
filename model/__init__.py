b8165fc3ad2ace725d737f6b89d0aadfb2a93736
Victor Iyiola Afolabi <javafolabi@gmail.com>
2018-01-02 13:35:09 +0100

    Model package

--- model/__init__.py
+++ model/__init__.py
@@ -2,8 +2,21 @@
   @author Victor I. Afolabi
   A.I. Engineer & Software developer
   javafolabi@gmail.com
-  
+
   Created on 21 December, 2017 @ 6:47 PM.
-  
+
   Copyright © 2017. Victor. All rights reserved.
 """
+from model.descriptor import Histogram
+from model.features import extract, save
+from model.searcher import Searcher
+
+__all__ = [
+    # model.descriptor
+    'Histogram',
+    # model.features
+    'extract',
+    'save',
+    # model.searcher
+    'Searcher',
+]
